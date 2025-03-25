0d15ebf3c7508aae6b9f215f47a69918632b9101
Pepe Alkalina <pepealkalina@MacBookPro.politecnicomalaga.com>
2025-03-25 10:08:31 +0100

    pruebas

--- ligas_kendo/ligas_kendo/urls.py
+++ ligas_kendo/ligas_kendo/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from prueba_django import views
 
 urlpatterns = [
+    path('', views.index, name="index"),
     path('admin/', admin.site.urls),
 ]
---
+++ ligas_kendo/prueba_django/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import HttpResponse, render
+
+# Create your views here.
+def index(request):
+    return HttpResponse("""
+        <h1>Inicio</h1>
+    """)
+    
